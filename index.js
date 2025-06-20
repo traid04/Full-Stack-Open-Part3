8ccb499f0ce777475f18f9012d9fcd6ba158b8b3
Traid <camposmatihas@gmail.com>
2025-06-19 21:11:41 -0300

    Parte 3 completa

--- index.js
+++ index.js
@@ -1,115 +1,116 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const cors = require('cors')
+dotenv.config()
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(morgan('tiny', { skip: (req) => req.method === 'POST' }))
+morgan.token('data', (req) => JSON.stringify(req.body))
 
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(morgan('tiny', {skip: (req, res) => req.method === 'POST'}));
-morgan.token('data', (req) => JSON.stringify(req.body));
-
-const Person = require('./models/person.js');
+const Person = require('./models/person.js')
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
-    if (error.name === "CastError") {
-        return response.status(400).send({error: error.message});
-    }
-    if (error.name === "ValidationError") {
-        return response.status(400).json({error: error.message});
-    }
-    next(error);
-};
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: error.message })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
 
 app.get('/api/persons', (request, response) => {
-    Person.find({})
-        .then(result => {
-            response.json(result);
-        })
-});
+  Person.find({})
+    .then(result => {
+      response.json(result)
+    })
+})
 
 app.get('/info', (request, response) => {
-    const date = new Date();
-    Person.find({})
-        .then(result => {
-            response.send(`<p>Phonebook has info for ${result.length} people </p> <p>${date}</p>`);
-        })
-});
+  const date = new Date()
+  Person.find({})
+    .then(result => {
+      response.send(`<p>Phonebook has info for ${result.length} people </p> <p>${date}</p>`)
+    })
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
-    Person.findById(id)
-        .then(result => {
-            if (result) {
-                response.json(result);
-            }
-            else {
-                response.status(404).end();
-            }
-        })
-        .catch(error => next(error))
-});
+  const id = request.params.id
+  Person.findById(id)
+    .then(result => {
+      if (result) {
+        response.json(result)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
-    Person.findByIdAndDelete(id)
-        .then(result => {
-            if (result) {
-                response.status(204).end();
-            }
-            else {
-                response.status(404).send({error: 'This user has already been removed from server'})
-            }
-        })
-        .catch(error => next(error))
-});
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    .then(result => {
+      if (result) {
+        response.status(204).end()
+      }
+      else {
+        response.status(404).send({ error: 'This user has already been removed from server' })
+      }
+    })
+    .catch(error => next(error))
+})
 
 
 app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :data'), (request, response, next) => {
-    const name = request.body.name;
-    const number = request.body.number;
-    if (!name && !number) {
-        response.status(400).json({error: 'Both fields are required'});
-        return;
-    }
-    else if (!name) {
-        response.status(400).json({error: 'The name field is required'});
-        return;
-    }
-    else if (!number) {
-        response.status(400).json({error: 'The number field is required'});
-        return;
-    }
-    const newPerson = new Person(request.body);
-    Person.findOne({name})
+  const name = request.body.name
+  const number = request.body.number
+  if (!name && !number) {
+    response.status(400).json({ error: 'Both fields are required' })
+    return
+  }
+  else if (!name) {
+    response.status(400).json({ error: 'The name field is required' })
+    return
+  }
+  else if (!number) {
+    response.status(400).json({ error: 'The number field is required' })
+    return
+  }
+  const newPerson = new Person(request.body)
+  Person.findOne({ name })
+    .then(result => {
+      if (result) {
+        response.status(400).json({ error: 'The name must be unique' })
+        return
+      }
+      newPerson.save()
         .then(result => {
-            if (result) {
-                response.status(400).json({error: 'The name must be unique'});
-                return;
-            }
-            newPerson.save()
-                .then(result => {
-                    response.status(200).json(result);
-                })
-                .catch(error => next(error));
+          response.status(200).json(result)
         })
-        .catch(error => next(error));
+        .catch(error => next(error))
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
-    const person = request.body;
-    Person.findByIdAndUpdate(id, person, {new: true, runValidators: true, context: 'query'})
-        .then(updatedPerson => {
-            response.json(updatedPerson);
-        })
-        .catch(error => next(error));
+  const id = request.params.id
+  const person = request.body
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on PORT: ${PORT}`)
+  console.log(`Server running on PORT: ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,30 +1,39 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
-const url = process.env.MONGO_URL;
-mongoose.set('strict', false);
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+const url = process.env.MONGO_URL
+mongoose.set('strict', false)
 mongoose.connect(url)
-    .then(connect => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(error => {
-        console.log(error.message);
-    });
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(error => {
+    console.log(error.message)
+  })
 mongoose.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
-    
+
 const personSchema = mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    }, 
-    number: String
-});
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d{8,}$/.test(v)
+      },
+      message: 'The phone number format is invalid'
+    },
+    required: true
+  }
+})
 
 module.exports = mongoose.model('Person', personSchema)
